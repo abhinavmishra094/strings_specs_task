ed12c691cd2445a4d4c81c821baa4b73fa9ce64b
Abhinav Mishra <abhinavmishra094@gmail.com>
2022-03-23 19:07:49 +0530

    average number added

--- main.go
+++ main.go
@@ -25,3 +25,16 @@ func testValidity(input string) bool {
 	}
 	return true
 }
+
+func averageNumber(inp string) float32 {
+	words := strings.Split(inp, "-")
+	nCount := 0
+	sum := 0
+	for _, word := range words {
+		if val, err := strconv.Atoi(word); err == nil {
+			nCount++
+			sum += val
+		}
+	}
+	return float32(sum) / float32(nCount)
+}
