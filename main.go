0f1c4719424b7c9eab675ff4b91deccabbe11a50
Abhinav Mishra <abhinavmishra094@gmail.com>
2022-03-23 19:09:49 +0530

    whole story and story stat added

--- main.go
+++ main.go
@@ -38,3 +38,53 @@ func averageNumber(inp string) float32 {
 	}
 	return float32(sum) / float32(nCount)
 }
+
+func wholeStory(inp string) string {
+	words := strings.Split(inp, "-")
+	story := ""
+	for _, word := range words {
+		if _, err := strconv.Atoi(word); err == nil {
+			continue
+		} else {
+			story += word + " "
+		}
+	}
+	return strings.TrimSpace(story)
+}
+
+func storyStats(inp string) (string, string, float32, []string) {
+	words := strings.Split(inp, "-")
+	var shortestWord string
+	var longestWord string
+	nWords := 0
+	totalLenWords := 0
+	var listWord []string
+	for _, word := range words {
+		if _, err := strconv.Atoi(word); err == nil {
+			continue
+		} else {
+			if shortestWord == "" || len(word) < len(shortestWord) {
+				shortestWord = word
+			}
+			if longestWord == "" || len(word) > len(longestWord) {
+				longestWord = word
+			}
+			nWords++
+			totalLenWords += len(word)
+		}
+	}
+	avgWordLength := float32(totalLenWords) / float32(nWords)
+	avgWordLengthRoundedUp := int(avgWordLength) + 1
+	avgWordLengthRoundedDown := int(avgWordLength)
+	for _, word := range words {
+		if _, err := strconv.Atoi(word); err == nil {
+			continue
+		} else {
+			if len(word) == avgWordLengthRoundedDown || len(word) == avgWordLengthRoundedUp {
+
+				listWord = append(listWord, word)
+			}
+		}
+	}
+	return shortestWord, longestWord, avgWordLength, listWord
+}
